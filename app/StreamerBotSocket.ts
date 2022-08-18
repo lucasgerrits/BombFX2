427c25f14aeb6d530c5c67b26a2c7f52536a9f27
lucasgerrits <lucasjgerrits@gmail.com>
2022-08-18 06:22:40 -0500

    Forgot to remove some logs

--- app/StreamerBotSocket.ts
+++ app/StreamerBotSocket.ts
@@ -82,8 +82,6 @@ export class StreamerBotSocket {
     }
 
     public async doAction(action: string, args: { } = { }): Promise<void> {
-        console.log(action);
-        console.log(args);
         let json: string = JSON.stringify(
             {
                 "request": "DoAction",
@@ -94,7 +92,6 @@ export class StreamerBotSocket {
                 "id": "" + this.createID()
             }
         );
-        console.log(json);
         this.socket.send(json);
     }
 
