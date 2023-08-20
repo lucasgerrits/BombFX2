b2f401d2349d60cb1189df58485af5567c0c0c52
lucasgerrits <lucasjgerrits@gmail.com>
2023-08-20 04:32:00 -0500

    Aggro: supplex source clone edit and filter backup

--- effects/aggro/attacks/suplex/Suplex.ts
+++ effects/aggro/attacks/suplex/Suplex.ts
@@ -5,39 +5,43 @@ import { Util } from "../../../../app/util/Util.js";
 declare var app: BombFX;
 
 export const suplex = async () => {
+    const effectScene: string = "Suplex";
+    const sceneClone: string = "Suplex Current Scene Clone";
+    const vertFlipFilter: string = "3D Effect - Vertical Flip";
+
     // Get current scene
     const currentScene: string = await app.obs.getCurrentSceneName();
 
     // Set current scene to source mirror properties
-    if (currentScene !== "Suplex") {
-        await app.obs.changeMirrorSource("Suplex Current Scene Mirror", currentScene);
+    if (currentScene !== effectScene) {
+        await app.obs.changeMirrorSource(sceneClone, currentScene);
 
         // Switch to suplex scene
-        await app.obs.setScene("Suplex");
+        await app.obs.setScene(effectScene);
     }
     await Util.sleep(500);
 
     // Play suplex video
-    await app.obs.showSource("Suplex Dot Webm", "Suplex");
+    await app.obs.showSource("Suplex Dot Webm", effectScene);
     await Util.sleep(6500);
 
     // Apply filter to move source mirror up
-    await app.obs.showFilter("Suplex", "Suplex - Up");
+    await app.obs.showFilter(effectScene, "Suplex - Up");
     await Util.sleep(300);
 
     // Apply filter to vertical flip current scene
-    await app.obs.showFilter("Suplex Current Scene Mirror", "Vertical Flip");
+    await app.obs.showFilter(sceneClone, vertFlipFilter);
     await Util.sleep(1250);
 
     // Undo flip fliter to return scene to proper vertical orientation
-    await app.obs.hideFilter("Suplex Current Scene Mirror", "Vertical Flip");
+    await app.obs.hideFilter(sceneClone, vertFlipFilter);
     await Util.sleep(2000);
 
     // Revert to original scene
-    if (currentScene !== "Suplex") {
+    if (currentScene !== effectScene) {
         await app.obs.setScene(currentScene);
     }
     
     // Turn off video
-    await app.obs.hideSource("Suplex Dot Webm", "Suplex");
+    await app.obs.hideSource("Suplex Dot Webm", effectScene);
 };
