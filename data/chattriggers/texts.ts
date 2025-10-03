48beaaf151774b0f7dd84a8588d0d7bffd2456dd
lucasgerrits <lucasjgerrits@gmail.com>
2025-10-03 13:08:33 -0500

    Update all misc chat triggers

--- data/chattriggers/texts.ts
+++ data/chattriggers/texts.ts
@@ -9,7 +9,7 @@ declare var app: BombFX;
 export const textTriggers: Array<ChatTriggerData> = [
     {
         trigger: "appreciate",
-        action: "Alternatives: thankful, grateful, flattered, glad, tickled pink, pleased, indebted, contented, ecstatic, delighted, much obliged"
+        action: "Alternatives: thankful, grateful, flattered, glad, tickled pink, pleased, indebted, contented, ecstatic, delighted, BUT DON'T USE MUCH OBLIGED"
     }, {
         trigger: "barkeep no",
         aliases: ["no barkeep", "barkeep bad", "bad barkeep", "milkbarkeep no", "no milkbarkeep", "milkbarkeep bad", "bad milkbarkeep"],
@@ -33,10 +33,6 @@ export const textTriggers: Array<ChatTriggerData> = [
         trigger: "!dmc",
         action: "I don't know who needs this information but the DMC HD collection is less than $10 on steam right now"
     }, {
-        trigger: "!faces",
-        aliases: ["!quarry"],
-        action: "is it just me or are the faces somehow a bit off in this game"
-    }, {
         trigger: "fuzzy pickles",
         cooldown: 60,
         announceCD: false,
@@ -47,9 +43,6 @@ export const textTriggers: Array<ChatTriggerData> = [
     }, {
         trigger: "!goodmorning",
         action: "I go to bed 😴 🛌 CFB 💯 🙏 I wake up 🌄 😁 It's CFB 😤 🙌"
-    }, {
-        trigger: "!greg",
-        action: "In case you are unaware, we are dating! :angry:"
     }, {
         trigger: "have you ever",
         action: "Have you ever thought about what it would be like to be a bee??"
@@ -62,13 +55,6 @@ export const textTriggers: Array<ChatTriggerData> = [
             }
         }
     }, {
-        trigger: "i click da cfb",
-        userLevel: UserLevel.Broadcaster,
-        permittedUsers: ["IClickDaCFB"],
-        cooldown: 30,
-        announceCD: false,
-        action: "They Click Da CFB"
-    }, {
         trigger: "itchy",
         action: async () => {
             app.twitch.bot.say("Itchy.");
@@ -79,43 +65,14 @@ export const textTriggers: Array<ChatTriggerData> = [
         trigger: "!itsnot",
         action: "\"It's not sexual!!\""
     }, {
-        trigger: "joel",
-        aliases: ["joeler", "joelest", "joelbutmywindowsxpiscrashing", "lethimjoel", "wholethimjoel"],
-        announceCD: false,
-        announcePrivs: false,
-        action: async (data) => {
-            if (data.user === "gengar_kiwami") {
-                app.twitch.chat.deleteMessage(data.extra.id);
-            }
-        }
-    }, {
-        trigger: "leclumpout",
-        action: "Hasty's tears fuel the Nightbo...Barkeep."
-    }, {
-        trigger: "!multi",
-        action: "https://kadgar.net/live/baloojay/carefreebomb"
-    }, {
-        trigger: "!nate",
-        aliases: ["!nate1280"],
-        action: "CREATOR NATE IS HERE bombHandsUp"
-    }, {
         trigger: "!promise",
         action: "\"cfb, you hot, moist garbage, you promised\" - MisterC_4"
     }, {
         trigger: "!re1",
         action: "This Is My Daily Campaign To Get Jammy To Purchase and Play Resident Evil Remake On GameCube"
     }, {
-        trigger: "!spacedock",
-        aliases: ["!spacedockin", "!spacedocking"],
-        action: "I WANNA GO!! SPACEDOCKIN!!!!"
-    }, {
         trigger: "!toxic",
         action: "You have been muted for receiving significantly more communications abuse reports than most players. Players will not see your communication unless they choose to unmute you."
-    }, {
-        trigger: "!weekstream",
-        aliases: ["!oneweek", "!uptime"],
-        action: "http://www.carefreebomb.com/nightbot/weekstream.php",
-        fetch: true
     }, {
         trigger: "!wetkiss",
         action: "A wet wet kiss to u bro. yaaayyyyy that sluspilsuspslsu"
